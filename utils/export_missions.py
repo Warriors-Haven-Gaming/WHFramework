8f2fc9575f439ef4ea83894ac49a6d402d61a100
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-03-24 10:57:25 -0400

    build: add utils/export_missions.py

---
+++ utils/export_missions.py
@@ -0,0 +1,123 @@
+"""Build mission files from the given repository branches.
+
+Requires Git to run. Index and working directory must be clean.
+
+"""
+
+import argparse
+import contextlib
+import shlex
+import subprocess
+from pathlib import Path
+from typing import Iterable, Iterator
+
+# FIXME: avoid hardcoding addon builder path
+ADDON_BUILDER = Path("C:/Program Files (x86)/Steam/steamapps/common/Arma 3 Tools/AddonBuilder/AddonBuilder.exe")
+DESTINATION = Path("build")
+DEFAULT_BRANCHES = ("main", "yulakia")
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-b",
+        "--branches",
+        default=list(DEFAULT_BRANCHES),
+        help=f"The branches to build (default: {' '.join(DEFAULT_BRANCHES)})",
+        nargs="*",
+    )
+    parser.add_argument(
+        "-d",
+        "--dest",
+        default=DESTINATION,
+        help=f"The destination directory (default: %(default)s)",
+        type=Path,
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print the commands to run instead of actually running them",
+    )
+
+    args = parser.parse_args()
+    branches: list[str] = args.branches
+    dest: Path = args.dest
+    dry_run: bool = args.dry_run
+
+    dest = dest.resolve()
+    dest.mkdir(exist_ok=True)
+
+    with contextlib.suppress(subprocess.CalledProcessError, AddonBuilderError):
+        for branch in branches:
+            build_branch(branch, dest, dry_run=dry_run)
+
+
+def build_branch(branch: str, dest_dir: Path, *, dry_run: bool) -> None:
+    check_call("git", "switch", branch, dry_run=dry_run)
+    for path in find_mission_directories():
+        build_mission(path, dest_dir, dry_run=dry_run)
+
+
+def find_mission_directories() -> Iterable[Path]:
+    return (p for p in Path().resolve().glob("WHFramework.*") if p.is_dir())
+
+
+def build_mission(src_dir: Path, dest_dir: Path, *, dry_run: bool) -> None:
+    with temp_patterns_file("*") as includes:
+        check_addon_builder_output(
+            ADDON_BUILDER,
+            str(src_dir),
+            str(dest_dir),
+            "-clear",
+            f"-include={shlex.quote(includes.name)}",
+            dry_run=dry_run,
+        )
+
+
+@contextlib.contextmanager
+def temp_patterns_file(*patterns: str) -> Iterator[Path]:
+    path = Path("includes.txt").resolve()
+    with path.open("x") as f:
+        f.write(";".join(patterns) + "\n")
+
+    try:
+        yield path
+    finally:
+        path.unlink()
+
+
+def check_call(*args: object, dry_run: bool) -> None:
+    str_args = [str(x) for x in args]
+    print("$", shlex.join(str_args))
+    if not dry_run:
+        subprocess.check_call(str_args, stdout=subprocess.DEVNULL)
+
+
+def check_output(*args: object, dry_run: bool) -> str | None:
+    str_args = [str(x) for x in args]
+    print("$", shlex.join(str_args))
+    if not dry_run:
+        return subprocess.check_output(str_args, text=True)
+
+
+def check_addon_builder_output(*args: object, dry_run: bool) -> str | None:
+    str_args = [str(x) for x in args]
+    print("$", shlex.join(str_args))
+    if dry_run:
+        return
+
+    out = subprocess.check_output(str_args, text=True)
+    print(out)
+    for line in out.splitlines():
+        error_index = line.find("[ERROR]: ")
+        if error_index >= 0:
+            raise AddonBuilderError(line[error_index + 9 :])
+
+    return out
+
+
+class AddonBuilderError(Exception): ...
+
+
+if __name__ == "__main__":
+    main()
