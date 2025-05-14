Warriors-Haven-Gaming/WHFramework

commit d5322138b72b4cf7216d6c7130b7ad98263dbbb0

feat: use mission radius in respawn distance checks

WHFramework.Altis/Functions/Missions/fn_msnMainAnnexRegion.sqf
@@ -17,6 +17,11 @@ Author:
 */
 params [["_location", locationNull]];
 
+private _minRadius = 250 + count allPlayers * 10 min 650;
+private _maxRadius = 500 + count allPlayers * 15 min 1100;
+private _radius = selectMax size _location * 2 max _minRadius min _maxRadius;
+_radius = _radius * WHF_missions_annex_size;
+
 if (_location isEqualTo locationNull) then {
     private _locations = nearestLocations [
         [worldSize / 2, worldSize / 2],
@@ -25,7 +30,7 @@ if (_location isEqualTo locationNull) then {
     ];
     _locations = _locations call WHF_fnc_arrayShuffle;
     {
-        if ([locationPosition _x, 1200] call WHF_fnc_isNearRespawn) then {continue};
+        if ([locationPosition _x, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         _location = _x;
         break;
     } forEach _locations;
@@ -36,11 +41,6 @@ if (_location isEqualTo locationNull) exitWith {
 
 private _center = locationPosition _location vectorMultiply [1, 1, 0];
 _center = _center vectorAdd [50 - random 100, 50 - random 100];
-
-private _minRadius = 250 + count allPlayers * 10 min 650;
-private _maxRadius = 500 + count allPlayers * 15 min 1100;
-private _radius = selectMax size _location * 2 max _minRadius min _maxRadius;
-_radius = _radius * WHF_missions_annex_size;
 private _area = [_center, _radius, _radius, 0, false];
 
 [_center, _radius] call WHF_fnc_msnMainAnnexRegionCompositions

WHFramework.Altis/Functions/Missions/fn_msnDestroyAAA.sqf
@@ -30,7 +30,7 @@ if (_center isEqualTo []) then {
     {
         _x params ["_pos"];
         _pos pushBack 0;
-        if ([_pos, 1000] call WHF_fnc_isNearRespawn) then {continue};
+        if ([_pos, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         if (_pos nearRoads 100 isNotEqualTo []) then {continue};
         if (_pos isFlatEmpty [-1, -1, 1, 40] isEqualTo []) then {continue};
         if ([_pos, _radius] call WHF_fnc_isNearUsedPosition) then {continue};

WHFramework.Altis/Functions/Missions/fn_msnDestroyArmor.sqf
@@ -30,7 +30,7 @@ if (_center isEqualTo []) then {
     {
         _x params ["_pos"];
         _pos pushBack 0;
-        if ([_pos, 1000] call WHF_fnc_isNearRespawn) then {continue};
+        if ([_pos, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         if (_pos nearRoads 100 isNotEqualTo []) then {continue};
         if (_pos isFlatEmpty [-1, -1, 1, 40] isEqualTo []) then {continue};
         if ([_pos, _radius] call WHF_fnc_isNearUsedPosition) then {continue};

WHFramework.Altis/Functions/Missions/fn_msnDestroyArtillery.sqf
@@ -30,7 +30,7 @@ if (_center isEqualTo []) then {
     {
         _x params ["_pos"];
         _pos pushBack 0;
-        if ([_pos, 1000] call WHF_fnc_isNearRespawn) then {continue};
+        if ([_pos, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         if (_pos nearRoads 100 isNotEqualTo []) then {continue};
         if (_pos isFlatEmpty [-1, -1, 1, 40] isEqualTo []) then {continue};
         if ([_pos, _radius] call WHF_fnc_isNearUsedPosition) then {continue};

WHFramework.Altis/Functions/Missions/fn_msnDestroyBarracks.sqf
@@ -47,7 +47,7 @@ if (_center isEqualTo []) then {
     {
         _x params ["_pos"];
         _pos pushBack 0;
-        if ([_pos, 1000] call WHF_fnc_isNearRespawn) then {continue};
+        if ([_pos, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         if (_pos nearRoads 100 isNotEqualTo []) then {continue};
         if (_pos isFlatEmpty [-1, -1, 0.5, 12] isEqualTo []) then {continue};
         if ([_pos, _radius] call WHF_fnc_isNearUsedPosition) then {continue};

WHFramework.Altis/Functions/Missions/fn_msnDestroyRoadblock.sqf
@@ -34,7 +34,7 @@ if (_center isEqualTo []) then {
     {
         _x params ["_pos"];
         _pos pushBack 0;
-        if ([_pos, 1000] call WHF_fnc_isNearRespawn) then {continue};
+        if ([_pos, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
         if (_pos isFlatEmpty [-1, -1, 1, 12] isEqualTo []) then {continue};
         if ([_pos, _radius + 200] call WHF_fnc_isNearUsedPosition) then {continue};
 

commit 6c7573e8cbd899a640998558357b020bc8936fea

fix: location radius calculation for annex mission
Regression from d5322138b72b4cf7216d6c7130b7ad98263dbbb0

WHFramework.Altis/Functions/Missions/fn_msnMainAnnexRegion.sqf
@@ -17,10 +17,20 @@ Author:
 */
 params [["_location", locationNull]];
 
-private _minRadius = 250 + count allPlayers * 10 min 650;
-private _maxRadius = 500 + count allPlayers * 15 min 1100;
-private _radius = selectMax size _location * 2 max _minRadius min _maxRadius;
-_radius = _radius * WHF_missions_annex_size;
+private _getRadius = {
+    params ["_location"];
+
+    private _minRadius = 250 + count allPlayers * 10 min 650;
+    private _maxRadius = 500 + count allPlayers * 15 min 1100;
+
+    private _radius = selectMax size _location * 2;
+    _radius = _radius max _minRadius min _maxRadius;
+    _radius = _radius * WHF_missions_annex_size;
+
+    _radius
+};
+
+private _radius = [_location] call _getRadius;
 
 if (_location isEqualTo locationNull) then {
     private _locations = nearestLocations [
@@ -30,7 +40,9 @@ if (_location isEqualTo locationNull) then {
     ];
     _locations = _locations call WHF_fnc_arrayShuffle;
     {
+        _radius = [_x] call _getRadius;
         if ([locationPosition _x, _radius + 1000] call WHF_fnc_isNearRespawn) then {continue};
+
         _location = _x;
         break;
     } forEach _locations;