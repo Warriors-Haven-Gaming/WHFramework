Warriors-Haven-Gaming/WHFramework

commit c7b1e39a1b8511ff049b0cbc27b0aa281624004f

refactor: wrap remoteExec arguments in arrays

WHFramework.Altis/Functions/APS/fn_simulateAPSLoop.sqf
@@ -28,7 +28,7 @@ private _showMessage = {
         _ammo,
         _vehicle getVariable ["WHF_aps_ammo_max", _ammo]
     ];
-    {_message remoteExec ["WHF_fnc_localizedCutText", _x]} forEach _players;
+    {[_message] remoteExec ["WHF_fnc_localizedCutText", _x]} forEach _players;
 };
 
 while {true} do {

WHFramework.Altis/Functions/Context/fn_initContextActionQuadbike.sqf
@@ -23,7 +23,7 @@ if (!hasInterface) exitWith {};
         WHF_quadbike setVariable ["WHF_vehicleLock_driver", ["uid", [getPlayerUID focusOn]], true];
         // playSound3D ["", WHF_quadbike];
 
-        WHF_quadbike remoteExec ["WHF_fnc_queueGCDeletion", 2];
+        [WHF_quadbike] remoteExec ["WHF_fnc_queueGCDeletion", 2];
     },
     nil,
     true,

WHFramework.Altis/Functions/Halo/fn_haloJump.sqf
@@ -51,7 +51,7 @@ private _units = _allUnits select {isNull objectParent _x};
 private _seed = floor random 1000000;
 private _players = flatten (_vehicles apply {crew _x}) select {isPlayer _x && {_x isNotEqualTo focusOn}};
 _seed spawn WHF_fnc_haloJumpCut;
-{_seed remoteExec ["WHF_fnc_haloJumpCut", _x]} forEach _players;
+{[_seed] remoteExec ["WHF_fnc_haloJumpCut", _x]} forEach _players;
 {[_x, _forEachIndex] spawn {
     params ["_unit", "_i"];
     sleep (0.1 * _i + random 0.5);

WHFramework.Altis/Functions/Missions/fn_msnMainAnnexRegionComms.sqf
@@ -61,8 +61,8 @@ private _guardGroup = [opfor, "standard", selectRandom [4, 6, 8], _pos, 20] call
 _groups pushBack _guardGroup;
 
 private _towerFunction = "WHF_fnc_msnMainAnnexRegionCommsTower";
-{_x remoteExec [_towerFunction, WHF_globalPlayerTarget, _x]} forEach _tower;
-{_x remoteExec ["WHF_fnc_addIntelAction", WHF_globalPlayerTarget, _x]} forEach _intel;
+{[_x] remoteExec [_towerFunction, WHF_globalPlayerTarget, _x]} forEach _tower;
+{[_x] remoteExec ["WHF_fnc_addIntelAction", WHF_globalPlayerTarget, _x]} forEach _intel;
 [_officer, "variable"] remoteExec ["WHF_fnc_addIntelAction", WHF_globalPlayerTarget, _officer];
 
 private _taskID = [

WHFramework.Altis/Functions/Prisoners/fn_detainUnitRequest.sqf
@@ -101,7 +101,7 @@ _target action ["Surrender", _target];
 [_target, ["", 0, 0, false]] remoteExec ["switchGesture"];
 
 if (isNil {_target getVariable "WHF_disableGC"}) then {
-    _target remoteExec ["WHF_fnc_queueGCDeletion", 2];
+    [_target] remoteExec ["WHF_fnc_queueGCDeletion", 2];
 };
 
 _target removeWeapon binocular _target;

WHFramework.Altis/Functions/Time/fn_requestSkipTime.sqf
@@ -20,7 +20,7 @@ params ["_player", "_timeOfDay"];
 if !([_player] call WHF_fnc_isPlayerRemoteExecuted) exitWith {};
 if (!WHF_requestSkipTime_enabled) exitWith {};
 if (!isNil "WHF_requestSkipTime_script" && {!scriptDone WHF_requestSkipTime_script}) exitWith {
-    "$STR_WHF_showSkipTimePending" remoteExec ["WHF_fnc_localizedHint", remoteExecutedOwner];
+    ["$STR_WHF_showSkipTimePending"] remoteExec ["WHF_fnc_localizedHint", remoteExecutedOwner];
 };
 
 private _last = missionNamespace getVariable ["WHF_requestSkipTime_last", time - WHF_requestSkipTime_cooldown];

WHFramework.Altis/Functions/Unflip/fn_unflipVehicle.sqf
@@ -30,9 +30,9 @@ if (_pos isEqualTo []) exitWith {
     _vehicle setVariable ["WHF_unflipVehicle_failedAt", time];
     private _message = "$STR_WHF_unflipVehicle_spacing";
     if (isRemoteExecuted) then {
-        _message remoteExec ["WHF_fnc_localizedHint", remoteExecutedOwner];
+        [_message] remoteExec ["WHF_fnc_localizedHint", remoteExecutedOwner];
     } else {
-        _message call WHF_fnc_localizedHint;
+        [_message] call WHF_fnc_localizedHint;
     };
 };
 

commit 836fc4ba884ce37a49282f33019a236958274f93

fix: a couple broken localization hints
Regression from c7b1e39a1b8511ff049b0cbc27b0aa281624004f

WHFramework.Altis/Functions/APS/fn_simulateAPSLoop.sqf
@@ -28,7 +28,7 @@ private _showMessage = {
         _ammo,
         _vehicle getVariable ["WHF_aps_ammo_max", _ammo]
     ];
-    {[_message] remoteExec ["WHF_fnc_localizedCutText", _x]} forEach _players;
+    {_message remoteExec ["WHF_fnc_localizedCutText", _x]} forEach _players;
 };
 
 while {true} do {

WHFramework.Altis/Functions/VehicleSpawner/fn_vehSpawnDespawn.sqf
@@ -23,7 +23,7 @@ if (!local _vehicle) exitWith {};
 private _reason = ["$STR_WHF_vehSpawnDespawnMessage", name _owner];
 {
     if (alive _x) then {moveOut _x} else {_vehicle deleteVehicleCrew _x};
-    if (isPlayer _x) then {[_reason] remoteExec ["WHF_fnc_localizedHint", _x]};
+    if (isPlayer _x) then {_reason remoteExec ["WHF_fnc_localizedHint", _x]};
 } forEach crew _vehicle;
 
 deleteVehicle _vehicle;