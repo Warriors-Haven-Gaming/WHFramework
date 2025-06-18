Warriors-Haven-Gaming/WHFramework

commit 879f33af04781247f2abb24931212256508f8aae

refactor: remove redundant area arguments

WHFramework.Altis/Functions/Halo/fn_haloJumpGUI.sqf
@@ -30,7 +30,7 @@ private _isEnemy = {
 };
 private _side = side group focusOn;
 private _radius = WHF_halo_enemy_distance;
-private _area = [getPosATL focusOn, _radius, _radius, 0, false];
+private _area = [getPosATL focusOn, _radius, _radius];
 private _units = _area nearEntities [["CAManBase"], false, false, true];
 if (_units findIf _isEnemy >= 0) exitWith {
     50 cutText [localize "$STR_WHF_haloJumpGUI_enemy", "PLAIN DOWN", 0.3];

WHFramework.Altis/Functions/GC/fn_garbageCollectorLoop.sqf
@@ -25,7 +25,7 @@ private _processDiscreetQueue = {
         if (_time < _startAt) then {continue};
 
         private _center = _objects # 0;
-        private _area = [getPosATL _center, _minDistance, _minDistance, 0, false];
+        private _area = [getPosATL _center, _minDistance, _minDistance];
         if ([_units, _area] call WHF_fnc_anyInArea) then {continue};
 
         _callback forEach _objects;

WHFramework.Altis/Functions/Markers/fn_createAreaMarker.sqf
@@ -22,7 +22,7 @@ Returns:
 
 Examples:
     (begin example)
-        [[], [[0,0,0], 100, 100, 0, false]] call WHF_fnc_creaAreaMarker;
+        [[], [[0,0,0], 100, 100]] call WHF_fnc_creaAreaMarker;
     (end)
 
 Author:
@@ -30,7 +30,7 @@ Author:
 
 */
 params ["_randomMarkerIDParams", "_area", ["_local", false]];
-_area params ["_center", "_a", "_b", "_angle", "_isRectangle"];
+_area params ["_center", "_a", "_b", ["_angle", 0], ["_isRectangle", false]];
 
 private _marker = [_randomMarkerIDParams, _center] call WHF_fnc_createLocalMarker;
 if (_marker isEqualTo "") exitWith {_marker};

WHFramework.Altis/Functions/Missions/fn_msnDefendAidSupplies.sqf
@@ -50,7 +50,7 @@ if (_center isEqualTo []) then {
             if ([_pos, _safeRadius] call WHF_fnc_isNearRespawn) then {continue};
             if (_pos nearRoads _radius isEqualTo []) then {continue};
 
-            private _area = [_pos, _safeRadius, _safeRadius, 0, false];
+            private _area = [_pos, _safeRadius, _safeRadius];
             if ([_units, _area] call WHF_fnc_anyInArea) then {continue};
 
             _center = _pos;

WHFramework.Altis/Functions/Missions/fn_msnDefendAidSuppliesStart.sqf
@@ -47,7 +47,7 @@ private _reinforceScript =
     spawn WHF_fnc_msnDefendAidSuppliesReinforcements;
 _scripts pushBack _reinforceScript;
 
-private _area = [_center, _radius * 1.5, _radius * 1.5, 0, false];
+private _area = [_center, _radius * 1.5, _radius * 1.5];
 private _areaMarker = [["WHF_msnDefendAidSupplies_"], _area, true] call WHF_fnc_createAreaMarker;
 _areaMarker setMarkerBrushLocal "FDiagonal";
 _areaMarker setMarkerColorLocal "ColorBlue";

WHFramework.Altis/Functions/Missions/fn_msnDefendAidSuppliesTheft.sqf
@@ -63,7 +63,7 @@ private _playersNearSupply = {
     params ["_supply"];
     allPlayers
         select {side group _x isEqualTo blufor}
-        inAreaArray [getPosATL _supply, _radius, _radius, 0, false]
+        inAreaArray [getPosATL _supply, _radius, _radius]
 };
 
 private _sideChat = {
@@ -86,7 +86,7 @@ while {true} do {
     {
         if (!alive _x) then {continue};
 
-        private _area = [getPosATL _x, 10, 10, 0, false];
+        private _area = [getPosATL _x, 10, 10];
         if !([_units, _area] call WHF_fnc_anyInArea) then {
             _supplyTheftAt set [_forEachIndex, -1];
             continue;

WHFramework.Altis/Functions/Missions/fn_msnDestroyAAA.sqf
@@ -43,7 +43,7 @@ if (_faction isEqualTo "") then {_faction = selectRandom (WHF_factions_pool get 
 private _standard = ["standard", _faction];
 private _supply = ["supply", _faction];
 
-private _area = [_center, _radius, _radius, 0, false];
+private _area = [_center, _radius, _radius];
 private _ruins = [];
 
 private _aaTypes = WHF_missions_aaa_types;

WHFramework.Altis/Functions/Missions/fn_msnDestroyArmor.sqf
@@ -42,7 +42,7 @@ if (_center isEqualTo []) exitWith {
 if (_faction isEqualTo "") then {_faction = selectRandom (WHF_factions_pool get opfor)};
 private _standard = ["standard", _faction];
 
-private _area = [_center, _radius, _radius, 0, false];
+private _area = [_center, _radius, _radius];
 
 private _objects = [];
 private _terrain = [];
@@ -129,7 +129,7 @@ if (count _vehicles < 1) exitWith {
 };
 
 private _taskCenter = _center getPos [20 + random 20, random 360];
-private _taskArea = [_taskCenter, _radius, _radius, 0, false];
+private _taskArea = [_taskCenter, _radius, _radius];
 private _areaMarker = [["WHF_msnDestroyArmor_"], _taskArea, true] call WHF_fnc_createAreaMarker;
 _areaMarker setMarkerBrushLocal "FDiagonal";
 _areaMarker setMarkerColorLocal "ColorRed";

commit 25882780e4127e7bcab0014f9898b1aa4fa128e8

fix: being able to halo jump near enemies
Regression in 879f33af04781247f2abb24931212256508f8aae

WHFramework.Altis/Functions/Halo/fn_haloJumpGUI.sqf
@@ -30,7 +30,7 @@ private _isEnemy = {
 };
 private _side = side group focusOn;
 private _radius = WHF_halo_enemy_distance;
-private _area = [getPosATL focusOn, _radius, _radius];
+private _area = [getPosATL focusOn, _radius, _radius, 0, false];
 private _units = _area nearEntities [["CAManBase"], false, false, true];
 if (_units findIf _isEnemy >= 0) exitWith {
     50 cutText [localize "$STR_WHF_haloJumpGUI_enemy", "PLAIN DOWN", 0.3];