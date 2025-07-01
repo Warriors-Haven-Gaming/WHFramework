7f68ad424a95bcadd4c5b8703025a02b2e25c36b
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2025-07-01 18:07:17 -0400

    feat: add main addon to mod

---
+++ mod/addons/main/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+    class whf_main {
+        name = "Warriors Haven Framework";
+        author = "thegamecracks";
+        url = "https://github.com/Warriors-Haven-Gaming/WHFramework";
+
+        requiredVersion = 2.20;
+        requiredAddons[] = {};
+        skipWhenMissingDependencies = 0;
+
+        units[] = {};
+    };
+};
